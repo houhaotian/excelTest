b9c61c4458959e11cc94ef2fd06c1b1132198d23
houhaotian <haotian_h@163.com>
2019-07-24 18:25:44 +0800

    init

---
+++ AxManager.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QHash>
+
+class QAxObject;
+
+class AxManager : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    AxManager(QObject *parent = 0);
+    ~AxManager();
+    bool Test(QString &path);
+
+
+    QString ConvertFromNumber(int number)
+    {
+        QString resultStr = "";
+        while (number > 0)
+        {
+            int k = number % 26;
+            if (k == 0)
+                k = 26;
+            resultStr.push_front(QChar(k + 64));
+            number = (number - k) / 26;
+        }
+
+        return resultStr;
+    }
+    //打开excel文件并返回表的个数，如果返回-1则打开失败
+    int openExcelFile(const QString &path);
+    bool closeExcelFile();
+    void loadData();
+
+private:
+    //存编号，目标值
+    QHash<QString, QString> m_hash;
+    
+    QAxObject *excel;    //本例中，excel设定为Excel文件的操作对象
+    QAxObject *workbooks;
+    QAxObject *workbook;  //Excel操作对象
+    QAxObject * worksheet;//工作表
+    QAxObject * usedrange;//有效的sheet工作范围
+};
---
+++ MainWindow.cpp
@@ -0,0 +1,16 @@
+﻿#include "MainWindow.h"
+#include "AxManager.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+
+    AxManager *axm = new AxManager(this);
+
+    axm->openExcelFile(QString("c:/Users/123/Desktop/excTest/sum.xls"));
+    axm->loadData();
+    axm->closeExcelFile();
+}
+
